219c61f5632e018af863508f32ba39a050d768a7
OctopusCosmic <zhan3447@purdue.edu>
2022-02-08 15:18:05 -0500

    update files_hand_coded

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+
+######### process the old version selected and frame into desired format ##########
 def get_content(my_location_raw, my_char_start, my_char_end,df): # state, document_number, paragraph_number
     my_location = my_location_raw.split("|")
     state = my_location[0]
@@ -30,7 +32,7 @@ def process_highlighted_labeled_content(state_abbr,df_f1):
     df_label_a_state["char_end"] = df_label_a_state["location"].apply(lambda x: get_paragraph_length(x,df_f1))
     df_label_a_state["char_start"] = df_label_a_state["char_start"].fillna(0).astype(int)
     # get the each highlighted labeled content by locations
-    df_label_a_state["content"] = df_label_a_state.apply(lambda row: get_content(row.location ,row.char_start ,row.char_end,df_f1), axis=1)
+    df_label_a_state["selected_content"] = df_label_a_state.apply(lambda row: get_content(row.location ,row.char_start ,row.char_end,df_f1), axis=1)
 
     return df_label_a_state
 
@@ -43,13 +45,29 @@ def main():
 
     # process to desired format
     df_label_a_state["Document number"] = df_label_a_state["location"].apply(lambda x: x.split("|")[1])
-    df_label_a_state["label"] = df_label_a_state["label"].apply(lambda x: x.replace(" ",""))
-    df_label_a_state=df_label_a_state.drop(columns=['row', "location"])
+    df_label_a_state["labeled frame"] = df_label_a_state["label"].apply(lambda x: x.replace(" ",""))
+    df_label_a_state=df_label_a_state.drop(columns=['row', "location","label"])
     df_label_a_state.to_csv(f"{path}AL_selected_content_with_label.csv")
+
+################################################################################
 
 main()
 
-
-
-
 ############## RAKE EXAMPLE #################
+def main2():
+    path = "/Users/zhangyuke/PycharmProjects/Climate_Media_beautifulsoup/hand_coded_articles_ready/"
+    df_label_a_state = pd.read_csv(f"{path}AL_selected_content_with_label.csv")
+    # This csv contains useful information
+    # Document number,
+    # content: selected content,
+    # label: frame
+
+
+
+
+
+
+
+
+
+
